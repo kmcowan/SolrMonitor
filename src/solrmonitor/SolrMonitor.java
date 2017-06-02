cce88dd1394cc0a136f627bb473fb56917562d7e
kmcowan <kmcowan@cprassoc.com>
2017-06-01 18:44:29 -0600

    add monitor loop time

--- src/solrmonitor/SolrMonitor.java
+++ src/solrmonitor/SolrMonitor.java
@@ -5,6 +5,9 @@
  */
 package solrmonitor;
 
+import java.io.File;
+import java.io.FileReader;
+import java.util.Properties;
 import solrmonitor.tasks.SolrPingTimerTask;
 
 /**
@@ -12,17 +15,22 @@ import solrmonitor.tasks.SolrPingTimerTask;
  * @author kevin
  */
 public class SolrMonitor {
-
+    private static final String propsFileName = "solr_monitor.properties";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         SolrPingTimerTask task = new SolrPingTimerTask();
+     
+        
         try{
+               Properties props = new Properties();
+        props.load(new FileReader(new File(propsFileName)));
+        long loopTime = Long.parseLong(props.getProperty("monitor.loop.time"));
          while(true){
-             System.out.println("PING SOLR...");
+             System.out.println("Monitor Loop Run...");
              task.run();
-             Thread.sleep(1000);
+             Thread.sleep(loopTime);
          }
         }catch(Exception e){
             e.printStackTrace();
